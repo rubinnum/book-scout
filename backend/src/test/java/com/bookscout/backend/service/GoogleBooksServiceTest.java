3f693d6e2ba81ac240ff9dd829d204d26c224038
rubinnum <romarypiak@gmail.com>
2024-09-08 15:10:48 +0200

    tesing for the non-empty response for all of the categories implemented

---
+++ backend/src/test/java/com/bookscout/backend/service/GoogleBooksServiceTest.java
@@ -0,0 +1,40 @@
+package com.bookscout.backend.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class GoogleBooksServiceTest {
+
+    @Autowired
+    private GoogleBooksService googleBooksService;
+
+    private static Stream<String> provideSubjectsForSearch() {
+        return Stream.of("antiques", "architecture", "art", "bibles", "biography", "body",
+                "business", "comics", "computers", "cooking", "crafts", "design", "drama",
+                "education", "family", "fiction", "games", "gardening", "health", "history",
+                "house", "humor", "juvenile", "law", "literary", "mathematics", "medical",
+                "music", "nature", "arts", "pets", "philosophy", "photography", "poetry",
+                "political", "psychology", "reference", "religion", "science", "social",
+                "sports", "technology", "transportation", "travel", "crime", "young");
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideSubjectsForSearch")
+    void searchPsychologyBooksIsNotEmpty(String subject) {
+        // When
+        String response = googleBooksService.searchBooksBySubject(subject);
+
+        // Then
+        assertThat(response).doesNotContain("totalItems\": 0");
+    }
+}
