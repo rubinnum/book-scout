9ed47f81b8eb522e5951b717ddf17e4291d130bd
rubinnum <romarypiak@gmail.com>
2024-09-25 23:27:53 +0200

    put request to update displayedBooks added to axios

--- frontend/src/components/BookCarousel/BookCarousel.jsx
+++ frontend/src/components/BookCarousel/BookCarousel.jsx
@@ -4,14 +4,19 @@ import React, {useState} from 'react';
 import {Button, Container} from "react-bootstrap";
 import BookCard from "../BookCard/BookCard.jsx";
 import {ChevronDown, ChevronUp} from 'lucide-react';
+import backend_api from "../../api/backend_api.js";
 
-function BookCarousel({books}) {
-    const [currentIndex, setCurrentIndex] = useState(0);
+function BookCarousel({books, currentIndex, setCurrentIndex, subject}) {
+
+    const updateDisplayedBooksProgress = async (subject) => {
+        console.log(await backend_api.put(`/progress/${subject}?booksDisplayed=${currentIndex + 1}`));
+    }
 
     const handleNext = () => {
         if (currentIndex < books.length - 1) {
             setCurrentIndex(currentIndex + 1);
         }
+        updateDisplayedBooksProgress(subject);
     };
 
     const handlePrevious = () => {
@@ -22,15 +27,15 @@ function BookCarousel({books}) {
 
     return (
         <Container className="d-flex flex-column align-items-center justify-content-center min-vh-100">
-            <Button variant="secondary" onClick={handlePrevious} className="mb-4 rounded-circle" disabled={currentIndex === 0}>
-                <ChevronUp size={24} />
+            <Button variant="secondary" onClick={handlePrevious} className="mb-4 rounded-circle"
+                    disabled={currentIndex === 0}>
+                <ChevronUp size={24}/>
             </Button>
             <div className="book-carousel-container">
-                <BookCard book={books[currentIndex]} />
+                <BookCard book={books[currentIndex]}/>
             </div>
-            
-            <Button variant="secondary" onClick={handleNext} className="mt-4 rounded-circle" disabled={currentIndex === books.length - 1}>
-                <ChevronDown size={24} />
+            <Button variant="secondary" onClick={handleNext} className="mt-4 rounded-circle">
+                <ChevronDown size={24}/>
             </Button>
         </Container>
     )
--- frontend/src/pages/CategoryBooks.jsx
+++ frontend/src/pages/CategoryBooks.jsx
@@ -7,25 +7,33 @@ import CustomSpinner from "../components/elements/CustomSpinner.jsx";
 function CategoryBooks() {
     const {subject} = useParams();
     const {books, setBooks} = useOutletContext();
+    const [currentIndex, setCurrentIndex] = useState(0);
 
     const [loading, setLoading] = useState(true);
 
     const getBooksBySubject = async (subject) => {
         const response = await backend_api.get(`/books/${subject}`);
         const loadedBooks = response.data.books
-        setBooks(loadedBooks);
+        setBooks(prevBooks => [...prevBooks, ...loadedBooks]);
         setLoading(false);
     }
 
     useEffect(() => {
-        getBooksBySubject(subject);
-    }, []);
+        if (books.length === 0 || (currentIndex === books.length - 1)) {
+            getBooksBySubject(subject);
+        }
+    }, [currentIndex, subject, setBooks]);
 
     if (loading) {
         return <CustomSpinner />
     }
 
-    return <BookCarousel books={books} />;
+    return <BookCarousel
+        books={books}
+        currentIndex={currentIndex}
+        setCurrentIndex={setCurrentIndex}
+        subject={subject}
+    />;
 }
 
 export default CategoryBooks;
