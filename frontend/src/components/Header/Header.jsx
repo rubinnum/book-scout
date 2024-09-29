ecb9c6d295a733c783a43d4200ada7ca71295d3b
rubinnum <romarypiak@gmail.com>
2024-09-29 23:58:11 +0200

    header styling improved

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import {Container, Nav, Navbar} from "react-bootstrap";
 import {Link} from "react-router-dom";
+import { Home, Book } from 'lucide-react';
+import "./Header.css"
 
 function Header() {
     return (
-        <Navbar expand="lg" className="bg-body-tertiary">
+        <Navbar expand="lg" bg="dark" variant="dark" className="custom-navbar">
             <Container>
-                <Nav.Link as={Link} to="/">
-                    <img src="/favicon.svg" width="40" height="40" className="d-inline-block align-top" alt="Book Scout Logo"/>
-                </Nav.Link>
-                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                <Navbar.Brand as={Link} to="/" className="d-flex align-items-center">
+                    <Book size={32} className="text-primary me-2" />
+                    <span>Book Scout</span>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link as={Link} to="/" className="text-dark">Home</Nav.Link>
+                    <Nav className="ms-auto">
+                        <Nav.Link as={Link} to="/" className="d-flex align-items-center">
+                            <Home size={18} className="me-1" />
+                            Home
+                        </Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
